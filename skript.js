4a5ae4e38a7a23bea4083167fccfa65feec83519
DraculaVlad666 <145560460+DraculaVlad666@users.noreply.github.com>
2024-11-04 23:17:19 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -20,7 +20,15 @@ class Game {
     constructor() {
         // Загружаем изображения
         this.backgroundImage = new Image();
-        this.backgroundImage.src = 'images/фон.png'; // Путь к фону
+        this.backgroundImage.src = 'background.png'; // Путь к фону
         this.cardImage = new Image();
         this.cardImage.src = 'images/карты.png'; // Путь к картам
-        this.diceImage =
+        this.diceImage = new Image();
+        this.diceImage.src = 'images/кубик.png'; // Путь к кубику
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.backgroundImage, 0, 0, ctx.canvas.width, ctx.canvas.height);
+        // Добавьте код для рисования карт и кубиков
+    }
+}
